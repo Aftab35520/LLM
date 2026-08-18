ef996d05d65979084ff3a6986aabf5f5725cffa9
aftabalam <161050400+Aftab35520@users.noreply.github.com>
2026-08-18 10:51:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def health_head():
 @app.post("/chat")
 def chat(data: ChatRequest):
     res = client.chat.completions.create(
-        model="Qwen3.6 27B",  # ✅ FIXED
+        model="qwen/qwen3.6-27b",  # ✅ FIXED
         messages=[{"role": "user", "content": data.prompt}],
     )
     return {"answer": res.choices[0].message.content}
